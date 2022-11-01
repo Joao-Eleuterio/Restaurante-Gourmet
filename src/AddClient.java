3d569aa9424b5139614e003978aa2c10df97d55b
Joao-Eleuterio <a22007237@alunos.ulht.pt>
2022-11-01 11:05:04 -0400

    Validacoes

--- src/AddClient.java
+++ src/AddClient.java
@@ -2,16 +2,11 @@ import java.rmi.*;
 import java.util.Scanner;
 
 public class AddClient {
+ 
   public static void main(String args[]) {
     try {
       String addServerURL = "rmi://" + args[0] + "/AddServer";
       AddServerIntf addServerIntf = (AddServerIntf) Naming.lookup(addServerURL);
-      /*
-       * System.out.println("The first number is: " + args[1]);
-       * double d1 = Double.valueOf(args[1]).doubleValue();
-       * System.out.println("The second number is: " + args[2]);
-       * double d2 = Double.valueOf(args[2]).doubleValue();
-       */
       do {
         System.out.println("----------------LOGIN------------------");
         System.out.println("|1 - faça login                       |");
